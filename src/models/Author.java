d4e9a25a18f50bc61d0986cb503daf2fd6d9f897
Emre Idaet <98050381+EmreIdaet@users.noreply.github.com>
2025-03-14 23:50:03 +0200

    init Author class

---
+++ src/models/Author.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Author {
+    private String firstName;
+    private String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
