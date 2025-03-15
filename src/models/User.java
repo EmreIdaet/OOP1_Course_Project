f3b5d40436435b3c02fc0f4d3219172ac6ed05a5
Emre Idaet <98050381+EmreIdaet@users.noreply.github.com>
2025-03-15 20:54:17 +0200

    init User class

---
+++ src/models/User.java
@@ -0,0 +1,13 @@
+package models;
+
+public class User {
+    private String username;
+    private String password;
+    private boolean isAdmin;
+
+    public User(String username, String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+}
