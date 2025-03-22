f016465347157f74b1324bee7b8998c80537db70
Emre Idaet <98050381+EmreIdaet@users.noreply.github.com>
2025-03-22 12:13:15 +0200

    init Genre enum

---
+++ src/enums/Genre.java
@@ -0,0 +1,6 @@
+package enums;
+
+public enum Genre {
+    FICTION, NON_FICTION, FANTASY, HORROR, SCIENCE_FICTION,
+    ROMANCE, MYSTERY, HISTORY, BIOGRAPHY
+}
--- src/models/Book.java
+++ src/models/Book.java
@@ -1,16 +1,18 @@
 package models;
 
+import enums.Genre;
+
 public class Book {
     private Author author;
     private String title;
-    private String genre;
+    private Genre genre;
     private String description;
     private int year;
     private String keywords;
     private double rating;
     private int isbn;
 
-    public Book(Author author, String title, String genre, String description, int year, String keywords, double rating, int isbn) {
+    public Book(Author author, String title, Genre genre, String description, int year, String keywords, double rating, int isbn) {
         this.author = author;
         this.title = title;
         this.genre = genre;
