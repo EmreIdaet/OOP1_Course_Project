a86632bc3157cab9bbc1501e5fb8bf18bf1bcd15
Emre Idaet <98050381+EmreIdaet@users.noreply.github.com>
2025-04-12 21:02:58 +0300

    Author class modify

--- src/models/Author.java
+++ src/models/Author.java
@@ -1,11 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Author {
     private String firstName;
     private String lastName;
+    private List<Book> books;
 
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.books = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return "Author: " + firstName + " " + lastName + "\nBooks: " + books;
     }
 }
