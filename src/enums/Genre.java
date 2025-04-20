e10955f6b3846e7e75edeff0f0e8d55a03bb6ebd
Emre Idaet <98050381+EmreIdaet@users.noreply.github.com>
2025-04-20 18:00:25 +0300

    modified enum Genre

--- src/enums/Genre.java
+++ src/enums/Genre.java
@@ -2,5 +2,13 @@ package enums;
 
 public enum Genre {
     FICTION, NON_FICTION, FANTASY, HORROR, SCIENCE_FICTION,
-    ROMANCE, MYSTERY, HISTORY, BIOGRAPHY
+    ROMANCE, MYSTERY, HISTORY, BIOGRAPHY, OTHER;
+
+    public static Genre fromString(String str) {
+        try {
+            return Genre.valueOf(str.toUpperCase().replace(" ", "_"));
+        } catch (IllegalArgumentException e) {
+            return OTHER;
+        }
+    }
 }
